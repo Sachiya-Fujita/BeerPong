638cb0c7f3d300c3fb40f06b943fc00d812313fc
Sachiya-Fujita <s4ch1mos@gmail.com>
2019-06-26 17:42:26 +0900

    Presenterで管理するように修正

--- BeerPong/Assets/Script/InGame/UiManager.cs
+++ BeerPong/Assets/Script/InGame/UiManager.cs
@@ -7,20 +7,39 @@ using UniRx;
 
 public class UiManager : Presenter
 {
+	[SerializeField]
+	private UiView _view;
+
+	[SerializeField]
+	private UiModel model;
+
 	public override IObservable<Unit> BindAsync()
 	{
-		throw new NotImplementedException();
+		Debug.Log("Bind");
+		_view.HomeButton.OnSafeClickAsObservable()
+			.Subscribe(_ => SceneUtility.Instance.OnButton("Menu"))
+			.AddTo(this);
+
+		_view.ResetButton.OnSafeClickAsObservable()
+			.Subscribe(_ => Debug.Log("reset"))//_view.PingPong.GetComponent<ThorwBall>().ResetPos())
+			.AddTo(this);
+
+		_view.TestButton.OnSafeClickAsObservable()
+			.Subscribe(_ => Debug.Log("test"))
+			.AddTo(this);
+
+		return Observable.Return(Unit.Default);
 	}
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        
-    }
+	{
+		BindAsync();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
